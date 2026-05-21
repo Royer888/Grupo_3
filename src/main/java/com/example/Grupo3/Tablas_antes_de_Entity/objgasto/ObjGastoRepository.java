11ab059a97fc2b02373ecb1c017b6eb8aa9d63c3
@Criss_UWU <crispaco66@gmail.com>
2026-05-21 16:54:35 -0400

    Migracion tabla organismo_fin a posgrest

--- src/main/java/com/example/Grupo3/Tablas_antes_de_Entity/objgasto/ObjGastoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Grupo3.Tablas_antes_de_Entity.objgasto;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ObjGastoRepository extends JpaRepository<ObjGasto, Long> {
-}
